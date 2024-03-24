cf1d8d3169a97cfc9257ccab0c51b821c9807032
Ali <75311052+AAVision@users.noreply.github.com>
2024-03-24 14:39:15 +0200

    This commit will add lazy load to images

--- src/app/components/aboutus/aboutus.component.ts
+++ src/app/components/aboutus/aboutus.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { LazyLoadImageModule } from 'ng-lazyload-image';
 
 @Component({
   selector: 'app-aboutus',
   standalone: true,
-  imports: [],
+  imports: [
+    LazyLoadImageModule
+  ],
   templateUrl: './aboutus.component.html',
   styleUrl: './aboutus.component.scss'
 })
 export class AboutusComponent {
-
+  defaultImage: string = "../../../assets/images/loader.gif"
 }
