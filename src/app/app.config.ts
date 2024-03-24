c0b7e9223676f3ff8375d75fca2e79f98a64360a
Ali <75311052+AAVision@users.noreply.github.com>
2024-03-24 15:27:11 +0200

    This commit will compress images and do some enhancements

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, withHashLocation, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -7,7 +7,7 @@ import { MarkdownModule } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes, withViewTransitions()),
+    provideRouter(routes, withViewTransitions(), withHashLocation()),
     provideHttpClient(),
     importProvidersFrom(MarkdownModule.forRoot())
   ]
